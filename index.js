962f373f6cf62ae58a2eb3a0d30f7b42f7212946
stillcorners <yheedev@gmail.com>
2024-08-09 21:24:16 +0900

    api 요청 수정

--- index.js
+++ index.js
@@ -1,58 +1,69 @@
-import https from 'https';
-import fs from 'fs';
-import axios from 'axios';
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// async function searchBlog(query) {
+//   const response = await fetch(`/search?query=${encodeURIComponent(query)}`);
+//   if (response.ok) {
+//     const data = await response.json();
+//     var items = data.items;
+//     var resultsContainer = document.getElementById('results');
+//     resultsContainer.innerHTML = '';
+//     items.forEach(item => {
+//       var title = item.title;
+//       var link = item.link;
+//       var description = item.description;
+//       var resultItem = document.createElement('div');
+//       resultItem.className = 'result__item';
+//       resultItem.innerHTML = `
+//                 <h3>${title}</h3>
+//                 <p>${description}</p>
+//                 <a href="${link}" target="_blank">Read more</a>
+//               `;
+//       resultsContainer.appendChild(resultItem);
+//     });
+//   } else {
+//     console.error('Error:', response.status);
+//   }
+// }
 
-// const SSL_CRT_FILE= .cert/server.crt
-// const SSL_KEY_FILE= .cert/server.key
+// import fs from 'fs';
+// import axios from 'axios';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const app = express();
-const port = 5000;
+// const app = express();
+// const port = 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname)));
+// app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
-app.get('/search/blog', async (req, res) => {
-  const query = req.query.query;
-  const api_url =
-    'https://openapi.naver.com/v1/search/blog.json?query=' +
-    encodeURIComponent(query) +
-    '&display=10&start=1&sort=random';
+// app.get('/search/blog', async (req, res) => {
+//   const query = req.query.query;
+//   const api_url =
+//     'https://openapi.naver.com/v1/search/blog.json?query=' +
+//     encodeURIComponent(query) +
+//     '&display=10&start=1&sort=random';
 
-  try {
-    const response = await axios.get(api_url, {
-      headers: {
-        'X-Naver-Client-Id': 'oINvcti2ijXhM9DxWau8',
-        'X-Naver-Client-Secret': 'laXayxxY8j',
-      },
-    });
+//   try {
+//     const response = await axios.get(api_url, {
+//       headers: {
+//         'X-Naver-Client-Id': 'oINvcti2ijXhM9DxWau8',
+//         'X-Naver-Client-Secret': 'laXayxxY8j',
+//       },
+//     });
 
-    if (response.status === 200) {
-      res.json(response.data);
-    } else {
-      res.status(response.status).send('Error');
-    }
-  } catch (error) {
-    res.status(500).send('Error');
-  }
-});
-
-const httpsOptions = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert'),
-};
-
-https.createServer(httpsOptions, app).listen(port, () => {
-  console.log(`HTTPS Server is running at https://localhost:${port}`);
-});
+//     if (response.status === 200) {
+//       res.json(response.data);
+//     } else {
+//       res.status(response.status).send('Error');
+//     }
+//   } catch (error) {
+//     res.status(500).send('Error');
+//   }
+// });
 
 //TODO
 
