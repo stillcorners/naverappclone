dd1fc8aeda17aae046d9cca28311d0fb6cd45ada
stillcorners <yheedev@gmail.com>
2024-08-12 20:04:42 +0900

    index.js에서 index.html 서빙, cors 설정 추가, try문 수정

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,60 +11,16 @@ const client_secret = 'laXayxxY8j';
 
 app.use(
   cors({
-    origin: 'https://stillcorners.github.io',
+    origin: [
+      'https://stillcorners.github.io',
+      'https://glacial-basin-92310-01f7dc392c47.herokuapp.com',
+    ],
     methods: ['GET'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
-app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="kr">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="description" content="네이버 모바일 앱 클론코딩" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>NAVER clone</title>
-        <script>
-          async function searchBlog() {
-            const query = document.getElementById('searchQuery').value;
-            try {
-              const response = await fetch(\`/search/blog?query=\${encodeURIComponent(query)}\`);
-              const data = await response.json();
-              displayResults(data);
-            } catch (error) {
-              console.error('Error:', error);
-            }
-          }
-
-          function displayResults(data) {
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = data.items
-              .map(
-                item => \`
-                  <div class="result-item">
-                    <h3>\${item.title}</h3>
-                    <p>\${item.description}</p>
-                    <a href="\${item.link}" target="_blank">Read more</a>
-                  </div>
-                \`
-              )
-              .join('');
-          }
-        </script>
-      </head>
-      <body>
-        <div>
-          <input type="text" id="searchQuery" placeholder="검색어를 입력해주세요." />
-          <button onclick="searchBlog()">검색</button>
-        </div>
-        <div id="results"></div>
-      </body>
-    </html>
-    `);
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/search/blog', async (req, res) => {
   const query = req.query.query;
