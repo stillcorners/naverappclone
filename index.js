0c5948562d02ee3313b4c547b328d39405865237
stillcorners <yheedev@gmail.com>
2024-12-13 18:24:30 +0900

    proxy 수정, 환경 변수 수정, axios/webpack 삭제

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-// import axios from 'axios';
-
-// 배포 환경에서는 dotenv를 사용해서 .env 파일에 있는 clientId, clientSecret를 가져옴
-// netlify 환경에서는 netlify의 환경 변수에 저장한 clientId, clientSecret를 가져옴 
-// if (process.env.NODE_ENV !== 'production') {
-  // const dotenv = require('dotenv');
-  // dotenv.config();
-// }
-
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
-// export const handler = async (event) => {
-  // const query = event.queryStringParameters?.query;
+export const handler = async (event) => {
+  const query = event.queryStringParameters?.query;
   const apiUrl = `https://openapi.naver.com/v1/search/blog?query=${encodeURIComponent(query)}&display=10&start=1&sort=date`;
 
   try {
@@ -23,19 +14,19 @@ const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
     });
     console.log(response.data);
     
-    // return {
-      // statusCode: 200,
-      // headers: {
-      //   'Content-Type': 'application/json',
-      //   'Access-Control-Allow-Origin': '*', 
-      // },
-      // body: JSON.stringify(response.data),
-    // };
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*', 
+      },
+      body: JSON.stringify(response.data),
+    };
   } catch (error) {
     console.error('Error:', error);
-    // return {
-    //   statusCode: error.response?.status || 500,
-    //   body: JSON.stringify({ message: error.message }),
-    // };
+    return {
+      statusCode: error.response?.status || 500,
+      body: JSON.stringify({ message: error.message }),
+    };
   }
-// };
+};
